806fbb7595b758792348559645c7fb65a5202872
ciaoca <ciaoca@gmail.com>
2013-07-11 09:28:33 +0800

    版本升级至 1.1
    增加 API 支持
    
    删除参数 title 的支持

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-﻿cxColor 是基于 jQuery 的颜色选择器插件，输出值为 HEX 格式的颜色值。
+﻿#jQuery cxColor
+
+cxColor 是基于 jQuery 的颜色选择器插件，输出值为 HEX 格式的颜色值。
 
 版本：
-+ jQuery v1.4.4+
-+ jQuery cxColor v1.0
 
-http://code.ciaoca.com/jquery/cxcolor/
+* jQuery v1.4.4+
+* jQuery cxColor v1.1
+
+文档：http://code.ciaoca.com/jquery/cxcolor/
+
+示例：http://code.ciaoca.com/jquery/cxcolor/demo/
--- js/jquery.cxcolor.js
+++ js/jquery.cxcolor.js
@@ -1,28 +1,39 @@
 /*!
- * cxColor 1.0
- * date: 2012-12-06
+ * cxColor 1.1
+ * http://code.ciaoca.com/
  * https://github.com/ciaoca/cxColor
- * (c) 2012 Ciaoca, http://ciaoca.com/
+ * E-mail: ciaoca@gmail.com
+ * Released under the MIT license
+ * Date: 2013-07-04
  */
 (function($){
 	$.fn.cxColor=function(settings){
 		if(this.length<1){return;};
 		settings=$.extend({},$.cxColor.defaults,settings);
 
-		var color_obj=this;
-		if(color_obj.val().length>0){
-			settings.color=color_obj.val();
+		var theColorPanel={
+			jqObj:this,
+			fn:{}
+		};
+		var reportColorPanel;
+
+		if(theColorPanel.jqObj.val().length>0){
+			settings.color=theColorPanel.jqObj.val();
 		};
 
 		// 创建选择器
-		var color_pane,block_bg,temp_html;
+		var color_now,color_pane,color_table,lock_bg,temp_html;
 		color_pane=$("<div></div>",{"class":"cxcolor"}).appendTo("body");
 
 		// 初始化选择器面板
 		var color_hex=["00","33","66","99","cc","ff"];
 		var spcolor_hex=["ff0000","00ff00","0000ff","ffff00","00ffff","ff00ff"];
+		
 
-		temp_html="<div class='title'>清除颜色</div><table>";
+		temp_html="<div class='panel_hd'><a class='reset' href='javascript://' rel='reset'>默认颜色</a><a class='clear' href='javascript://' rel='clear'>清除</a></div>";
+		color_pane.html(temp_html);
+
+		temp_html="";
 		for(var i=0;i<2;i++){
 			for(var j=0;j<6;j++){
 				temp_html+="<tr>";
@@ -40,77 +51,110 @@
 				};
 			};
 		};
-		temp_html+="</table>";
-		color_pane.html(temp_html);
+		color_table=$("<table></table>").html(temp_html).appendTo(color_pane);
 		
 		// 背景遮挡层
-		block_bg=$("<div></div>").css({"display":"none","position":"absolute","top":"0","left":"0","background":"#fff","opacity":"0","z-index":"999"}).appendTo("body");
+		lock_bg=$("<div></div>",{"class":"cxcolor_lock"}).appendTo("body");
 
-		var colorShow=function(){
+		theColorPanel.fn.show=function(){
 			var doc_w=document.body.clientWidth;
 			var doc_h=document.body.clientHeight;
 			var pane_w=color_pane.outerWidth();
 			var pane_h=color_pane.outerHeight();
-			var pane_top=color_obj.offset().top;
-			var pane_left=color_obj.offset().left;
-			var obj_w=color_obj.outerWidth();
-			var obj_h=color_obj.outerHeight();
+			var pane_top=theColorPanel.jqObj.offset().top;
+			var pane_left=theColorPanel.jqObj.offset().left;
+			var obj_w=theColorPanel.jqObj.outerWidth();
+			var obj_h=theColorPanel.jqObj.outerHeight();
 			
 			pane_top=((pane_top+pane_h+obj_h)>doc_h) ? pane_top-pane_h : pane_top+obj_h;
 			pane_left=((pane_left+pane_w)>doc_w) ? pane_left-(pane_w-obj_w) : pane_left;
 
 			color_pane.css({"top":pane_top,"left":pane_left}).show();
-			block_bg.css({width:doc_w,height:doc_h}).show();
-		};
-
-		// 更改颜色函数
-		var colorThis=function(c){
-			color_obj.val(c).css("backgroundColor",c);
-			if(settings.title!=null){
-				$(settings.title).css("color",c);
-			};
+			lock_bg.css({width:doc_w,height:doc_h}).show();
 		};
 
 		// 关闭日期函数
-		var colorExit=function(){
+		theColorPanel.fn.hide=function(){
 			color_pane.hide();
-			block_bg.hide();
+			lock_bg.hide();
 		};
 
-		var color_title,color_table;
-		color_title=color_pane.find(".title");
-		color_table=color_pane.find("table");
+		// 更改颜色函数
+		theColorPanel.fn.change=function(c){
+			color_now=c;
+			theColorPanel.jqObj.val(color_now).css("backgroundColor",color_now);
+			theColorPanel.jqObj.trigger("change");
+			theColorPanel.fn.hide();
+		};
+
+		// 设置或获取颜色
+		theColorPanel.fn.setColor=function(c){
+			if(!c){
+				return color_now;
+			}else{
+				theColorPanel.fn.change(c);
+			};
+		};
+
+		// 还原颜色
+		theColorPanel.fn.reset=function(){
+			theColorPanel.fn.change(settings.color);
+		};
+
+		// 清除颜色
+		theColorPanel.fn.clear=function(){
+			theColorPanel.fn.change("");
+		};
+
+		// 面板 <a> 事件
+		color_pane.delegate("a","click",function(){
+			if(!this.rel){return};
+
+			var _rel=this.rel;
+			switch(_rel){
+				case "reset":
+					theColorPanel.fn.reset()
+					return false;
+					break
+				case "clear":
+					theColorPanel.fn.clear()
+					return false;
+					break
+			};
+		});
 
 		// 选择颜色事件
 		color_table.delegate("td","click",function(){
 			var color_val=this.title;
-			colorThis(color_val);
-			colorExit();
+			theColorPanel.fn.change(color_val);
 		});
 
 		// 显示面板事件
-		color_obj.bind("click",function(){
-			colorShow();
-		});
-
-		// 清除颜色事件
-		color_title.bind("click",function(){
-			colorThis(settings.color);
-			colorExit();
+		theColorPanel.jqObj.bind("click",function(){
+			theColorPanel.fn.show();
 		});
 
 		// 关闭面板事件
-		block_bg.bind("click",function(){
-			colorExit();
+		lock_bg.bind("click",function(){
+			theColorPanel.fn.hide();
 		});
 
 		// 第一次初始化
-		colorThis(settings.color);
+		theColorPanel.fn.change(settings.color);
+
+		reportColorPanel={
+			jqubj:theColorPanel.jqobj,
+			show:theColorPanel.fn.show,
+			hide:theColorPanel.fn.hide,
+			color:theColorPanel.fn.setColor,
+			reset:theColorPanel.fn.reset,
+			clear:theColorPanel.fn.clear
+		}
+		return reportColorPanel;
 	};
 	
 	// 默认值
 	$.cxColor={defaults:{
-		color:"#000000",	// 默认颜色
-		title:null			// 关联的元素
+		color:"#000000"		// 默认颜色
 	}};
 })(jQuery);
